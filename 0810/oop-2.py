62eedf9e388426845a4236cbef614e6cf5297b67
Zac <a19860101@gmail.com>
2025-08-10 10:48:43 +0800

    20250810

--- 0810/oop-2.py
+++ 0810/oop-2.py
@@ -9,40 +9,34 @@ class User:
         self.level = 1
 
     def attack(self, v):
-        if v>30:
-            self.exp += 100
-        else:
-            self.exp += 1
+        if v > 80: self.exp += 64
+        elif v > 60: self.exp += 48
+        elif v > 40: self.exp += 32
+        elif v > 20: self.exp += 16
+        else: self.exp += 8
         print(f'你攻擊對手，對方扣{v}HP')
+        print(f'目前經驗值:{self.exp}')
+        print('---')
         self.up()
 
     def up(self):
         if self.exp > 100 + (self.level*10):
-            self.exp = 0
+            self.exp = self.exp - (100 + (self.level*10))
             self.level += 1
             print(f'等級提升，目前等級為{self.level}')
+            print(f'目前經驗值:{self.exp}')
+            print('-------------------')
+        else:
+            print(f'目前等級:{self.level}')
+            print('-------------------')
+
+import random
 
 hello = User('射手','hello')
-print(hello)
-print(hello.job)
-print(hello.name)
-print(hello.hp)
-print(hello.mp)
-print(hello.exp)
-hello.attack(15)
-print(hello.exp)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
-hello.attack(100)
+
+for i in range(10):
+    v = random.randint(1, 100)
+    hello.attack(v)
+
+
 
