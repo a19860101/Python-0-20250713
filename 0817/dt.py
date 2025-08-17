4ee87db537a2662be96c223fd7a833e5926d1e5d
Zac <a19860101@gmail.com>
2025-08-17 11:06:32 +0800

    20250817

---
+++ 0817/dt.py
@@ -0,0 +1,34 @@
+# import datetime as dt
+# t = dt.datetime.today()
+
+from datetime import datetime, timezone, timedelta
+# t = datetime.today()
+
+# from datetime import datetime as dt
+#
+# t = dt.today()
+# print(t)
+# print(datetime.datetime.today())
+# print(datetime.datetime.now())
+# print(datetime.datetime.utcnow())
+
+# print(t.date)
+# print(t.year)
+# print(t.month)
+# print(t.day)
+# print(t.weekday())
+# print(t.isoweekday())
+#
+# print(t.hour)
+# print(t.minute)
+# print(t.second)
+
+# 設定時區 timezone
+# tz = datetime.timezone(datetime.timedelta(hours=0))
+# # print(datetime.datetime.now(tz))
+
+# tz = dt.timezone(dt.timedelta(hours=0))
+# print(dt.datetime.now(tz))
+
+tz = timezone(timedelta(hours=0))
+print(datetime.now(tz))
