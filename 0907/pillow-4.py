04426cbf62dd9f998763d01883db19ceb4e1a7a5
Zac <a19860101@gmail.com>
2025-09-07 16:12:53 +0800

    20250907

--- 0907/pillow-4.py
+++ 0907/pillow-4.py
@@ -2,6 +2,9 @@ import glob
 from PIL import Image
 import os
 
+ww = input('請輸入縮圖寬度:')
+q = int(input('請輸入縮圖品質:'))
+
 images = glob.glob('./images/*.[Jj][Pp][Gg]') + glob.glob('./images/*.[Jj][Pp][Ee][Gg]') + glob.glob('./images/*.[Pp][Nn][Gg]') + glob.glob('./images/*.[Gg][Ii][Ff]')
 # i=0
 for i, image in enumerate(images):
@@ -11,7 +14,7 @@ for i, image in enumerate(images):
     w,h = img.size
     # print(w, h)
 
-    w_s = 600
+    w_s = int(ww)
     h_s = int((w_s * h) / w)
 
     os.makedirs('output', exist_ok=True)
@@ -29,9 +32,13 @@ for i, image in enumerate(images):
     # img.save('./output/002.jpg', quality=75)
     # img_s.save(f'./output/0{i+1}.jpg', quality=75)
     # img_s.save(f'./output/{imgname[:-4]}{ext}', quality=75)
-    img_s.save(f'./output/{name}.{ext}', quality=75)
+    img_s.save(f'./output/{name}.{ext}', quality=q)
 
     # i+=1
+
+    # pip install pyinstaller
+
+    # pyinstaller 檔案路徑
 
 
 
